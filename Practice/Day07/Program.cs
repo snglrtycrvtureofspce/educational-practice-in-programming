c20ed9b1c18e534a8fe45b46e00fd6623ea3f036
Aleksandr <asp1rantzenevich@gmail.com>
2022-10-22 17:13:54 +0300

    #

---
+++ Practice/Day07/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("Введите номер: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                {
+                    char[] arr = new char[15];
+                    Console.WriteLine("Начальный массив: ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = (char)rnd.Next(0x0410, 0x44F); // https://unicode-table.com/ru/
+                        Console.WriteLine(arr[i] + "\t");
+                    }
+                    Console.WriteLine("Конечный массив: ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (char.IsLower(arr[i]))
+                        {
+                            Console.WriteLine(char.ToUpper(arr[i]));
+                        }
+
+                        if (char.IsUpper(arr[i]))
+                        {
+                            Console.WriteLine(char.ToLower(arr[i]));
+                        }
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    char[] arr = new char[15];
+                    Console.WriteLine("Начальный массив: ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = (char)rnd.Next(0x0410, 0x44F); // https://unicode-table.com/ru/
+                        Console.WriteLine(arr[i] + "\t");
+                    }
+
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Exit...");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Day07/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Day07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day07")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("93388e5b-c06b-4b6d-b9c1-a7e0c196b991")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
