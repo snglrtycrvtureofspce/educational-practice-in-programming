5a3db097a4b4fa36eb73553c57a8b9214edcba07
Aleksandr <asp1rantzenevich@gmail.com>
2022-10-26 18:05:02 +0300

    A#

--- Practice/Day07/Program.cs
+++ Practice/Day07/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -41,14 +42,162 @@ namespace Day07
                 }
                 case 2:
                 {
-                    char[] arr = new char[15];
+                    string[] arr = "Просто обычный пример теkста! Проверим kаkие слова будут содержать букву k".Split();
+                    Console.WriteLine("Исходный текст: ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Console.Write(arr[i] + " ");
+                    }
+                    Console.WriteLine("\nБуква k найдена:");
+                    foreach (string arr2 in arr.Where(k => k.Contains("k")))
+                    {
+                        Console.WriteLine(arr2);
+                    }
+                    break;
+                }
+                case 3:
+                {
+                    char[] arr = new char[16];
                     Console.WriteLine("Начальный массив: ");
                     for (int i = 0; i < arr.Length; i++)
                     {
-                        arr[i] = (char)rnd.Next(0x0410, 0x44F); // https://unicode-table.com/ru/
+                        arr[i] = (char)rnd.Next(0x0030, 0x0070); // https://unicode-table.com/ru/
                         Console.WriteLine(arr[i] + "\t");
                     }
-
+                    char maxvalue = '0';
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (char.IsNumber(arr[i]))
+                        {
+                            if (arr[i] > maxvalue)
+                            {
+                                maxvalue = arr[i];
+                            }
+                        }
+                    }
+                    Console.WriteLine("Наибольшее целое число - " + maxvalue);
+                    break;
+                }
+                case 4:
+                {
+                    string[] arr = "Просто обычный пример теkста! Нам просто нужно добавить символ в конец строки!".Split();
+                    Console.WriteLine("Исходный текст: ");
+                    foreach (var t in arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                    Console.WriteLine("\nПолучившийся текст: ");
+                    for (int i = 0; i < 1; i++)
+                    {
+                        arr[arr.Length - 1] += "<>";
+                    }
+                    foreach (var t in arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+                }
+                case 5:
+                {
+                    string[] arr = "Просто обычный пример теkста! Нам просто нужно добавить символ в конец строки!".Split();
+                    Console.WriteLine("Исходный текст: ");
+                    foreach (var t in arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                    Console.WriteLine("\nПолучившийся текст: ");
+                    for (int i = 0; i < 1; i++)
+                    {
+                        arr[arr.Length - 1] += $"{arr.Length}";
+                    }
+                    foreach (var t in arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+                }
+                case 6:
+                {
+                    string[] arr = "Давайте заменим каждый символ: ! индексом этого символа, ! , пров ! f ка !".Split();
+                    Console.WriteLine("Исходный текст: ");
+                    foreach (var t in arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                    Console.WriteLine("\nПолучившийся текст: ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] == "!")
+                        {
+                            arr[i] = $"{i}";
+                        }
+                    }
+                    foreach (var t in arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+                }
+                case 7:
+                {
+                    Console.Write("Введите римскую цифру (от I до X): ");
+                    string arr = Console.ReadLine();
+                    switch (arr)
+                    {
+                        case "I":
+                        {
+                            Console.WriteLine("1");
+                            break;
+                        }
+                        case "II":
+                        {
+                            Console.WriteLine("2");
+                            break;
+                        }
+                        case "III":
+                        {
+                            Console.WriteLine("3");
+                            break;
+                        }
+                        case "IV":
+                        {
+                            Console.WriteLine("4");
+                            break;
+                        }
+                        case "V":
+                        {
+                            Console.WriteLine("5");
+                            break;
+                        }
+                        case "VI":
+                        {
+                            Console.WriteLine("6");
+                            break;
+                        }
+                        case "VII":
+                        {
+                            Console.WriteLine("7");
+                            break;
+                        }
+                        case "VIII":
+                        {
+                            Console.WriteLine("8");
+                            break;
+                        }
+                        case "IX":
+                        {
+                            Console.WriteLine("9");
+                            break;
+                        }
+                        case "X":
+                        {
+                            Console.WriteLine("10");
+                            break;
+                        }
+                    }
                     break;
                 }
                 default:
---
+++ Practice/Day08/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                {
+                    string s = "4190672H011PB3";
+                    Console.WriteLine("Исходная строка: "+ s);
+                    Regex regex = new Regex("[1-6]{1}[0-9]{6}[A-Z]{1}[0-9]{3}[GB|PB|BA|BI]{2}[0-9]{1}");
+                    Console.WriteLine("Ввод корректен? - " + regex.IsMatch(s));
+                    break;
+                }
+                case 2:
+                {
+                    string temp = null;
+                    string s = "http://www.regexcookbook.com";
+                    Regex regex = new Regex("^(https?:\\/\\/)?([\\w-]{1,32}\\.[\\w-]{1,32})[^\\s@]*");
+                    if (regex.IsMatch(s))
+                    {
+                        temp = $"<a href=\"{s}\">{s}</a>";
+                    }
+                    Console.WriteLine(temp);
+        
+                    break;
+                }
+                case 3:
+                {
+                    string s = "+375(33)358-94-89;15.11.2004;3sec";
+                    Console.WriteLine("Исходный текст: " + s);
+                    Regex regex = new Regex("(?i)[0|1|2|3|4]{1}[sec|min]{3}");
+                    Console.WriteLine("Время связи меньше 5-ти секунд: " + regex.IsMatch(s));
+                    break;
+                }
+                case 4:
+                {
+                    string path = "C:\\Users\\snglrtycrvtureofspce\\Documents\\snglrtycrvtureofspce\\GitHub\\" +
+                                  "educational-practice-in-programming\\Practice\\Day08\\example.txt";
+                    FileInfo fi = new FileInfo(path);   
+                    if (fi.Exists)
+                    {
+                        fi.Delete();
+                    }
+
+                    using (FileStream fs = fi.Create())
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("Its key is {3F2504E0-4F89-UDD3-9A0C-0305E82C3301}.");
+                        fs.Write(info, 0, info.Length);
+                    }
+
+                    Regex regex = new Regex("(?i)\\b[0-9a-zA-Z]{8}\\-[0-9a-zA-Z]{4}\\-[0-9a-zA-Z]{4}\\-[0-9a-zA-Z]{4}\\-[0-9a-zA-Z]{12}\\b");
+
+                    using (StreamReader sr = fi.OpenText())
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(s);
+                            Console.WriteLine("GUID - "+ regex.IsMatch(s));
+                        }
+                    }
+                    break;
+                }
+                case 5:
+                {                        
+                    string r =
+                        @"(?i)\b" +
+                        @"[0-9a-zA-Z]{8}\-" +
+                        @"[0-9a-zA-Z]{4}\-" +
+                        @"[0-9a-zA-Z]{4}\-" +
+                        @"[0-9a-zA-Z]{4}\-" +
+                        @"[0-9a-zA-Z]{12}" +
+                        @"\b";
+                    string text = "Its key is {3F2504E0-4F89-UDD3-9A0C-0305E82C3301}.";
+                    Console.WriteLine(Regex.Match(text, r).Index); // 12
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Exit...");
+                    break;
+                }
+            }
+        }
+    }
+}
