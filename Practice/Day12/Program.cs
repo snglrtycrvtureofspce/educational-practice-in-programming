cda9a410c1061662416eac43025f68665b64339c
Aleksandr <asp1rantzenevich@gmail.com>
2022-11-26 01:07:24 +0300

    #

---
+++ Practice/Day12/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                {
+                    Triangle a = new Triangle(3, 4, 5);
+                    Console.WriteLine(a.ToString());
+                    Console.WriteLine($"Является ли треугольником? - {a.isTriangle()}");
+                    Console.WriteLine($"Периметр: {a.Perimeter()}");
+                    Console.WriteLine($"Площадь: {a.Area(3)}");
+                    Console.WriteLine("Работа индексатора: ");
+                    Console.WriteLine("Первый индекс: " + a[0]);
+                    Console.WriteLine("Второй индекс: " + a[1]);
+                    Console.WriteLine("Третий индекс: " + a[2]);
+                    Console.WriteLine("Четвёртый индекс (которого не должно существовать): " + a[3]);
+                    Console.WriteLine("Работа перегрузок: ");
+                    Console.WriteLine(a++);
+                    Console.WriteLine(a--);
+                    if (a)
+                    {
+                        Console.WriteLine("Треугольник с такими сторонами существует.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Треугольник с такими сторонами не существует.");
+                    }
+                    Console.WriteLine("Умножаем на 2: " + a * 2);
+                    break;
+                }
+                case 2:
+                {
+                    
+                    Triangle a = new Triangle(4, 5, 6);
+                    Console.WriteLine(a.ToString());
+                    Console.WriteLine("Свойства: ");
+                    a.A = 2;
+                    Console.WriteLine("Заменяем Сторону а на 2: " + a.A);
+                    Console.WriteLine(a.ToString());
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Exit...");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Day12/Triangle.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Day12
+{
+    public enum Frencuency {Равносторонний, Прямоугольный, Равнобедренный}
+    public class Triangle
+    {
+        public DateTime datecreate = DateTime.Now;
+        public Frencuency typeoftriangle;
+        private int _a, _b, _c;
+        public Triangle(){}
+
+        public Triangle(int a, int b, int c)
+        {
+            this._a = a;
+            this._b = b;
+            this._c = c;
+            if (a == b && a == c)
+            {
+                this.typeoftriangle = Frencuency.Равносторонний;
+            }
+            else if (a == b || b == c || a == c)
+            {
+                this.typeoftriangle = Frencuency.Равнобедренный;
+            }
+            else
+            {
+                this.typeoftriangle = Frencuency.Прямоугольный;
+                
+            }
+        }
+        public int A
+        {
+            set => this._a = value;
+            get => this._a;
+        }
+
+        private int B
+        {
+            set => this._b = value;
+            get => this._b;
+        }
+
+        public int C
+        {
+            set => this._c = value;
+            get => this._c;
+        }
+        public string this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return Convert.ToString(this._a);
+                    case 1:
+                        return Convert.ToString(this._b);
+                    case 2:
+                        return Convert.ToString(this._c);
+                    default:
+                        return "Ошибка! Неверный индекс.";
+                }
+            }
+        }
+
+        public static Triangle operator ++(Triangle temp)
+        {
+            return new Triangle(++temp._a, ++temp._b, ++temp._c);
+        }
+        public static Triangle operator --(Triangle temp)
+        {
+            return new Triangle(--temp._a, --temp._b, --temp._c);
+        }
+
+        public static bool operator true(Triangle temp)
+        {
+            return temp.isTriangle();
+        }
+
+        public static bool operator false(Triangle temp)
+        {
+            return temp.isTriangle();
+        }
+
+        public static Triangle operator *(Triangle temp, int scalar)
+        {
+            return new Triangle(temp._a *= scalar, temp._b *= scalar, temp._c *= scalar);
+        }
+
+        public int Perimeter()
+        {
+            return _a + _b + _c;
+        }
+            
+        public double Area(int R)
+        {
+            return 2 * Math.Pow(R, 2) * Math.Sign(_a) * Math.Sin(_b) * Math.Sin(_c);
+        }
+
+        public bool isTriangle()
+        {
+            if (_a + _b > _c && _a + _c > _b && _b + _c > _a)
+            {
+                return true;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public override string ToString()
+        {
+            return $"Информация о треугольнике:\nДата создания: {datecreate}\nСторона a: {this._a}\nСторона b: {this._b}\nСторона c: {this._c}\nТип треугольника:{this.typeoftriangle}";
+        }
+    }
+}
